09aa3421f3eace88d7d8d703a309a02f42ce66b7
Yuma <yumaito0528@gmail.com>
2021-01-20 02:33:27 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
 import logo from './logo.svg';
 import './App.css';
+import { NavigationBar } from './components/NavigationBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.link}>
+      <NavigationBar>
+      <Text>
+        test
+      </Text>
+      </NavigationBar>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#eeeeee"
+  },
+  link: {
+    margin: 16,
+  },
+  linkText: {
+    color: "#191970",
+    fontSize: 24,
+  },
+});
 export default App;
